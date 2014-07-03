67bc50b014b7c06638b612775cf741cc1e5e3767
James B Kemp <james@bibble.com>
2014-07-03 12:12:34 +0100

    added home controller and search function

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function() {
+
+	$('#search-button').on('click', function(event) {
+
+		// event.preventDefault();
+		console.log("button-pressed");
+		var search_terms = $("#search-box").val();
+		if (search_terms !== "") {
+			console.log(search_terms);
+			$("#search-box").val("");
+		};
+	});
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root to: 'home#index'
   resources :books, :authors
 
   # The priority is based upon order of creation: first created -> highest priority.
